123fbde8c2e6b4bf23383573d08358ed0abb10e5
Pr0FItt <dmitrijpetruhin3@gmail.com>
2025-03-27 19:52:39 +0300

    Основная задача
    Нашёл задачу на сайте LeetCode, написал там своё решение, сайт его принял. Следующий этап, добавить эту функцию в полноценную программу

---
+++ agroCifra/agroCifra.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        int counter = arr.size();
+        for (int num : arr) {
+            for (int i = 0; i < arr.size(); i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    if ((arr[j] * arr[i]) == num) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+};
