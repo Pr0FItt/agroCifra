fc9160df4309efe9cb9ec471f685c7e759dc44ae
Pr0FItt <dmitrijpetruhin3@gmail.com>
2025-04-01 21:27:35 +0300

    Update function
    Предыдущая версия работала частично, исправлены ошибки

--- agroCifra/agroCifra.cpp
+++ agroCifra/agroCifra.cpp
@@ -1,16 +1,36 @@
-﻿class Solution {
+﻿#include <vector>
+#include <algorithm>
+#include <unordered_map>
+class Solution {
 public:
-    int numFactoredBinaryTrees(vector<int>& arr) {
-        int counter = arr.size();
+    int numFactoredBinaryTrees(std::vector<int>& arr) {
+        const int MOD = 1e9 + 7;
+        std::sort(arr.begin(), arr.end());
+        std::unordered_map<int, int> index;
+        std::unordered_map<int, long long> trees;
+
         for (int num : arr) {
-            for (int i = 0; i < arr.size(); i++) {
-                for (int j = 0; j < arr.size(); j++) {
-                    if ((arr[j] * arr[i]) == num) {
-                        counter++;
+            trees[num] = 1;
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            index[arr[i]] = i;
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.size(); j++) {
+                if (arr[i] % arr[j] == 0) {
+                    int factor = arr[i] / arr[j];
+                    if (index.count(factor)) {
+                        trees[arr[i]] = (trees[arr[i]] + trees[arr[j]] * trees[factor]) % MOD;
                     }
                 }
             }
         }
-        return counter;
+        int sum;
+        for (auto [ind, num] : trees) {
+            sum = (sum + num) % MOD;
+        }
+        return sum;
     }
 };
